b1dac41911b1217da724a999e1bba4309f5f9f2b
Wiktor Wandachowicz <wiktor.wandachowicz@p.lodz.pl>
2017-11-20 16:50:46 +0100

    Klasa BloggingContext

---
+++ BlogData/BloggingContext.cs
@@ -0,0 +1,16 @@
+﻿using BlogModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogData
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
