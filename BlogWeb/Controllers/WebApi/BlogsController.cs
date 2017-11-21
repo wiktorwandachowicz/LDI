710242e96c897608543b0d0959367397ffadb81a
Wiktor Wandachowicz <wiktor.wandachowicz@p.lodz.pl>
2017-11-21 18:42:33 +0100

    Kontroler Web API: BlogsController

---
+++ BlogWeb/Controllers/WebApi/BlogsController.cs
@@ -0,0 +1,22 @@
+﻿using BlogData;
+using BlogModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BlogWeb.Controllers.WebApi
+{
+    public class BlogsController : ApiController
+    {
+        public IEnumerable<Blog> GetAllBlogs()
+        {
+            using (var repository = new BlogRepository())
+            {
+                return repository.GetAll().ToList();
+            }
+        }
+    }
+}
