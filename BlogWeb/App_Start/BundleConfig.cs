9d60821a213400b67e0169cf910a0a57bbe132e9
Wiktor Wandachowicz <wiktor.wandachowicz@p.lodz.pl>
2019-02-24 17:49:40 +0100

    Aktualizacja wersji pakietów dla projektów, zmiana wersji Visual Studio 2017 do 15.9.4

--- BlogWeb/App_Start/BundleConfig.cs
+++ BlogWeb/App_Start/BundleConfig.cs
@@ -11,17 +11,13 @@ namespace BlogWeb
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
             // Użyj wersji deweloperskiej biblioteki Modernizr do nauki i opracowywania rozwiązań. Następnie, kiedy wszystko będzie
             // gotowe do produkcji, użyj narzędzia do kompilowania ze strony https://modernizr.com, aby wybrać wyłącznie potrzebne testy.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
